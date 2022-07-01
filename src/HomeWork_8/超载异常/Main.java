9cf2b1e7887f8cff0014343bf38f1c7b72fb0c1b
linkaifeng <kaifenglin@126.com>
2022-07-01 16:34:34 +0800

    homework

--- src/HomeWork_8/超载异常/Main.java
+++ src/HomeWork_8/超载异常/Main.java
@@ -1,5 +1,15 @@
 package HomeWork_8.超载异常;
 
-class
+class OverLoadException extends Exception{
+    String message;
+    public OverLoadException(double n){
+        message="无法再装载重量是"+n+"吨的集装箱";
+    }
+    public void showMessage(){
+        System.out.println(this.message);
+    }
+
+}
+class CargoShip
 public class Main {
 }
